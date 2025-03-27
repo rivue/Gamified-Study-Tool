36aa95aa72bfa11dc0bd66cf48b05f5aa53623b8
Will Gunter <jameswgunterlll@gmail.com>
2025-03-27 02:51:34 -0400

    commented local domain and uncommented domain

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -205,8 +205,8 @@ router.afterEach(() => {
     loading.finish()
 })
 
-axios.defaults.baseURL = 'http://localhost:5000';
-// axios.defaults.baseURL = 'https://rivue.ai';
+// axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://rivue.ai';
 
 axios.defaults.withCredentials = true;
 
