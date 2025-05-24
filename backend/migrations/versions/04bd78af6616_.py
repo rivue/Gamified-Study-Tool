c4f1f69f1c2064a2a0024cba27a70adb64375b67
Will Gunter <jameswgunterlll@gmail.com>
2025-05-24 15:59:03 -0400

    empty file

---
+++ backend/migrations/versions/04bd78af6616_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 04bd78af6616
+Revises: c7209aac4ac0
+Create Date: 2025-05-23 10:46:31.640995
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '04bd78af6616'
+down_revision = 'c7209aac4ac0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
